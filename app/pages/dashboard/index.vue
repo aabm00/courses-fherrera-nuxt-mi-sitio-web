fe1fb1ebf27be92969288356e95acbfcaf5d1499
Toni <aabm00@gmail.com>
2026-04-07 18:44:34 +0200

    30. Organizacion de la carpeta pages

--- app/pages/dashboard/index.vue
+++ app/pages/dashboard/index.vue
@@ -1,12 +1,8 @@
 <script setup lang="ts">
-
-  definePageMeta({
-    layout: 'dashboard-layout'
-  })
-  
 </script>
 
 <template>
   <div>Page: Dashboard</div>
 </template>
+
 <style scoped></style>
---
+++ app/pages/dashboard/products/index.vue
@@ -0,0 +1,7 @@
+
+
+<template>
+    <div>
+        <h3>Products Dashboard</h3>
+    </div>
+</template>
