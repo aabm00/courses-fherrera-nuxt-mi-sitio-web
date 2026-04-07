9763a9edced8659a9f11f7ca5388817b6b62bd85
Toni <aabm00@gmail.com>
2026-04-07 19:11:37 +0200

    31. SEO Metatags -Configuraciones globales_1

--- app/pages/(public)/about.vue
+++ app/pages/(public)/about.vue
@@ -2,9 +2,20 @@
 <script lang="ts" setup>
 
 useSeoMeta({
-  title: "About | mi tienda de servicios",
-  description: "Cualquier cosa del about",
-  ogImage:"/og-image.png",
+  title: 'About | Mi tienda de servicios',
+  description:
+    'Bienvenido a la página principal de Mi tienda de servicios. Descubre nuestros productos y servicios.',
+  ogTitle: 'About | Mi tienda de servicios',
+  ogDescription:
+    'Bienvenido a la página principal de Mi tienda de servicios. Descubre nuestros productos y servicios.',
+  ogType: 'website',
+  ogUrl: 'https://tudominio.com/',
+  ogImage: '/og-image.png',
+  twitterCard: 'summary_large_image',
+  twitterTitle: 'About | Mi tienda de servicios',
+  twitterDescription:
+    'Bienvenido a la página principal de Mi tienda de servicios. Descubre nuestros productos y servicios.',
+  twitterImage: '/og-image.png',
 })
 
 </script>
